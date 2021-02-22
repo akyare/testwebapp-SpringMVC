447efadecde820e2809b579903f6dde8e1f76193
akyare <akyare@hotmail.com>
2021-02-22 07:13:43 +0100

    Adding lougout functionnality.

--- src/main/java/com/example/teswebapp/web/controllers/UserController.java
+++ src/main/java/com/example/teswebapp/web/controllers/UserController.java
@@ -9,11 +9,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Slf4j
@@ -43,6 +46,15 @@ public class UserController {
         model.addAttribute("users", userRepository.findAll());
 
         return "index";
+    }
+
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/index"; //You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 
     @GetMapping("/signup")
