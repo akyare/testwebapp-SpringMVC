658d57091ea41498f424a7c92d1a58d16e153650
akyare <akyare@hotmail.com>
2021-03-02 07:52:04 +0100

    Adding script schema.

--- src/main/scripts/create-schema-mysql.sql
+++ src/main/scripts/create-schema-mysql.sql
@@ -1,31 +1,40 @@
-create table hibernate_sequence (next_val bigint) engine=InnoDB;
-insert into hibernate_sequence values ( 1 );
+create table if not exists hibernate_sequence
+(
+    next_val bigint null
+);
 
 create table if not exists users
 (
     id bigint not null
         primary key,
     email varchar(255) null,
-    is_writer bit null,
-    name varchar(20) null,
+    enabled bit null,
     password varchar(255) null,
-    username varchar(20) null,
-    enabled tinyint default 1 not null,
+    is_writer bit null,
+    name varchar(255) null,
+    username varchar(255) null,
     constraint users_username_uindex
         unique (username)
 );
 
 create table if not exists authorities
 (
-    id bigint auto_increment
+    id bigint not null
         primary key,
     authority varchar(255) null,
     username varchar(255) null,
-    constraint unique_index
-        unique (username, authority),
-    constraint authorities_users_username_fk
+    constraint fk_authorities_users
         foreign key (username) references users (username)
 );
 
-
+create table if not exists verification_token
+(
+    id bigint not null
+        primary key,
+    expiry_date datetime null,
+    token varchar(255) null,
+    user_id bigint null,
+    constraint fk_veriftoken_users
+        foreign key (user_id) references users (id)
+);
 
